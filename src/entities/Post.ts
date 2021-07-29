c2e6da0bdf3763e327022c2fc0272b3094b69975
Anthony Kuang <kuanganthony@gmail.com>
2021-07-29 05:42:58 -0700

    GraphQL implementation in resolvers

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -1,16 +1,22 @@
 import { Entity, PrimaryKey, Property } from '@mikro-orm/core';
+import { Field, ObjectType } from 'type-graphql';
 
+@ObjectType()
 @Entity() // corresponds to a database table
 export class Post {
+  @Field() // exposing this field to GraphQL schema
   @PrimaryKey()
   id!: number;
 
+  @Field(() => String)
   @Property({ type: 'date' })
   createdAt = new Date(); // standard rows that most tables should have
 
+  @Field(() => String)
   @Property({ type: 'date', onUpdate: () => new Date() }) // only updates due to the MikroORM onUpdate hook
   updatedAt = new Date();
 
+  @Field()
   @Property({ type: 'text' })
   title!: string;
 }
